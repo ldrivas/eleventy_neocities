c077fd638ad5a35ea68acbe803ee6cfbbeea89f7
ldrivas <lucien.drivas@gmail.com>
2023-05-23 19:53:15 +0930

    updated background colour

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,18 @@
 document.addEventListener('click', function(event) {
-    var spark = document.createElement('div');
-    spark.className = 'spark';
-    spark.style.left = (event.clientX - 5) + 'px';
-    spark.style.top = (event.clientY - 5) + 'px';
-    document.body.appendChild(spark);
-
-    setTimeout(function() {
-        document.body.removeChild(spark);
-    }, 1000);
-});
+    var target = event.target;
+ 
+    // Check if the clicked element is a link or a descendant of a link
+    var isLink = target.tagName.toLowerCase() === 'a' || target.closest('a');
+ 
+    if (!isLink) {
+        var spark = document.createElement('div');
+        spark.className = 'spark';
+        spark.style.left = (event.clientX - 5 + window.pageXOffset) + 'px'; // Add window.pageXOffset
+        spark.style.top = (event.clientY - 5 + window.pageYOffset) + 'px'; // Add window.pageYOffset
+        document.body.appendChild(spark);
+ 
+        setTimeout(function() {
+            document.body.removeChild(spark);
+        }, 1000);
+    }
+ });
