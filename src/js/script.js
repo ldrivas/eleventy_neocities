b2d91cd4636205a21f7a823100169f582dc9b94c
ldrivas <lucien.drivas@gmail.com>
2023-07-12 22:17:50 +0930

    Updated

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,34 @@
 /* animation for cursor */
 
-document.addEventListener('click', function(event) {
-    var target = event.target;
- 
-    // Check if the clicked element is a link or a descendant of a link
-    var isLink = target.tagName.toLowerCase() === 'a' || target.closest('a');
- 
-    if (!isLink) {
+document.addEventListener('DOMContentLoaded', function() {
+  // Get the current page URL
+  var currentPage = window.location.pathname;
+
+  // Check if the current page is the panorama page
+  var isPanoramaPage = currentPage.includes('streetviewhindmarsh');
+
+  // Animation for cursor (excluding the panorama page)
+  if (!isPanoramaPage) {
+    document.addEventListener('click', function(event) {
+      var target = event.target;
+
+      // Check if the clicked element is a link or a descendant of a link
+      var isLink = target.tagName.toLowerCase() === 'a' || target.closest('a');
+
+      if (!isLink) {
         var spark = document.createElement('div');
         spark.className = 'spark';
-        spark.style.left = (event.clientX - 5 + window.pageXOffset) + 'px'; // Add window.pageXOffset
-        spark.style.top = (event.clientY - 5 + window.pageYOffset) + 'px'; // Add window.pageYOffset
+        spark.style.left = (event.clientX - 5 + window.pageXOffset) + 'px';
+        spark.style.top = (event.clientY - 5 + window.pageYOffset) + 'px';
         document.body.appendChild(spark);
- 
+
         setTimeout(function() {
-            document.body.removeChild(spark);
+          document.body.removeChild(spark);
         }, 1000);
-    }
- });
+      }
+    });
+  }
+});
 
  /* map image */
 
